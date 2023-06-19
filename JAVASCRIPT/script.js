0434ef8c57f022803d04c6c26ba26d7ae884b358
Kamil Justyński <kamil.justynski1998@gmail.com>
2023-06-19 19:01:34 +0200

    add functions

--- JAVASCRIPT/script.js
+++ JAVASCRIPT/script.js
@@ -3,32 +3,32 @@ const currencyElement = document.querySelector(".js--currency");
 const resultElement = document.querySelector(".js--result");
 const buttonElement = document.querySelector(".js--form");
 
-let currencyUSD = 4.18;
-let currencyEUR = 4.47;
-let currencyGBP = 5.19;
-let currencyCHF = 4.61;
+{
+  const calculate = (amount, currency) => {
+    let currencyUSD = 4.18;
+    let currencyEUR = 4.47;
+    let currencyGBP = 5.19;
+    let currencyCHF = 4.61;
+    switch (currency) {
+      case "USD":
+        return amount / currencyUSD;
+      case "EUR":
+        return amount / currencyEUR;
+      case "GBP":
+        return amount / currencyGBP;
+      case "CHF":
+        return amount / currencyCHF;
+    }
+  };
 
-buttonElement.addEventListener("submit", (e) => {
-  e.preventDefault();
-
-  let amount = +inputElement.value;
-  let currency = currencyElement.value;
-  let result;
-
-  switch (currency) {
-    case "USD":
-      result = amount / currencyUSD;
-      break;
-    case "EUR":
-      result = amount / currencyEUR;
-      break;
-    case "GBP":
-      result = amount / currencyGBP;
-      break;
-    case "CHF":
-      result = amount / currencyCHF;
-      break;
-  }
-
-  resultElement.innerText = ` ${result.toFixed(2)} ${currency}`
-});
+  const init = () => {
+    buttonElement.addEventListener("submit", (e) => {
+      e.preventDefault();
+      let amount = +inputElement.value;
+      let currency = currencyElement.value;
+      let result = calculate(amount, currency);
+      resultElement.innerText = ` ${result.toFixed(2)} ${currency}`;
+    });
+  };
+  init();
+}
